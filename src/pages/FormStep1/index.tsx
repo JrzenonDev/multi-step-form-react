bb0de7f9754bade6c5cb511c5edaa6e8ca81889e
JrzenonDev <jrobertoonb@gmail.com>
2022-05-25 17:44:01 -0300

    create method handleNameChange in FormStep1

--- src/pages/FormStep1/index.tsx
+++ src/pages/FormStep1/index.tsx
@@ -1,19 +1,36 @@
 import { useHistory } from 'react-router-dom'
 import * as C from './styles'
+import { useForm, FormActions } from '../../contexts/FormContext'
 import { Theme } from '../../components/Theme'
+import { ChangeEvent, useEffect } from 'react'
 
 export const FormStep1 = () => {
 
+  const { state, dispatch } = useForm()
   const history = useHistory()
+
+  useEffect(() => {
+    dispatch({
+      type: FormActions.setCurrentStep,
+      payload: 1
+    })
+  }, [])
 
   const handleNextStep = () => {
     history.push('/step2')
   }
 
+  const handleNameChange = (e: ChangeEvent<HTMLInputElement>) => {
+    dispatch({
+      type: FormActions.setName,
+      payload: e.target.value
+    })
+  }
+
   return (
     <Theme>
       <C.Container>
-        <p>Passo 1</p>
+        <p>Passo 1 - {state.currentStep}</p>
         <h1>Vamos começar com o seu nome</h1>
         <p>Preencha o campo abaixo com o seu nome completo.</p>
 
@@ -21,7 +38,12 @@ export const FormStep1 = () => {
 
         <label>
           Seu nome completo
-          <input type="text" autoFocus />
+          <input
+            type="text"
+            autoFocus
+            value={state.name}
+            onChange={handleNameChange}
+          />
         </label>
 
         <button onClick={handleNextStep}>Próximo</button>
