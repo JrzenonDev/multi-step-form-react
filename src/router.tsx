15d45b6a64c7853089a061d1abf1d167cac3dd4d
JrzenonDev <jrobertoonb@gmail.com>
2022-05-25 23:02:40 -0300

    correction name route

--- src/router.tsx
+++ src/router.tsx
@@ -7,8 +7,8 @@ export const Router = () => {
   return (
     <BrowserRouter>
       <Route path='/' exact component={FormStep1} />
-      <Route path='/spet2' component={FormStep2} />
-      <Route path='/sptep3' component={FormStep3} />
+      <Route path='/step2' component={FormStep2} />
+      <Route path='/step3' component={FormStep3} />
     </BrowserRouter>
   )
 }
